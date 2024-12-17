7c14cd81847f4b985af51970cece61e1ec37cc5f
cazorladev <cazorladevnet@gmail.com>
2024-12-17 16:19:33 -0500

    Renderización del tablero

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -2,43 +2,74 @@
 #include <vector>
 #include <ctime>
 #include <cstdlib>
+#include <csignal>
 
 using namespace std;
 
-// Configuración Inicial
 const int WIDTH = 10, HEIGHT = 20;
+enum Tetromino {I, J, L, O, S, T, Z};
 
-// Enumeración para las piezas tetromino
-enum Tetronimo {I, J, L, O, S, T, Z};
+const vector<vector<vector<int>>> TETROMINO_SHAPES = {
+   {{1, 1, 1, 1}},
+   {{1, 1}, {1, 1}},
+   {{0, 1, 0}, {1, 1, 1}},
+   {{1, 0, 0}, {1, 1, 1}},
+   {{0, 0, 1}, {1, 1, 1}},
+   {{0, 1, 1}, {1, 1, 0}},
+   {{1, 1, 0}, {0, 1, 1}}
+};
 
 struct Piece {
    vector<vector<int>> shape;
    int x, y;
 
-   Piece(Tetromino type) : x(WIDTH / 2), y(0) {}
+   Piece(Tetromino type)
+      : shape(TETROMINO_SHAPES[type]), x(WIDTH / 2 - shape[0].size() / 2), y(0) {}
 };
 
-// Variables globales
 vector<vector<int>> board(HEIGHT, vector<int>(WIDTH, 0));
 
 // Funciones
-void displayBoard();
+void displayTitleScreen();
+void resetGame();
+void signalHandler();
+void renderBoard();
+Piece* createRandomPiece();
 
 int main() {
-
-   for (int i = 0; i < 10; i++) {
-      fill(board[i].begin(), board[i].end(), 0);
-   }
-   cout << "¡Bienvenido a Tetris!" << endl;
-   displayBoard();
-
+   signal(SIGINT, signalHandler);
+   displayTitleScreen();
+   cin.get(); // Esperar Enter
+   resetGame();
+   cout << "Juego iniciado.\n";
    return 0;
 }
 
-void displayBoard() {
-   for (int i = 0; i < HEIGHT; i++) {
-      for (int j = 0; j < WIDTH; j++) {
-         cout << board[i][j] << "..";
+void displayTitleScreen() {
+   cout  << "=====================================\n"
+         << "=         BIENVENIDO A TETRIS       =\n"
+         << "=====================================\n\n"
+         << "Presiona Enter para comenzar...\n";
+}
+
+void resetGame() {
+   board.assign(HEIGHT, vector<int>(WIDTH, 0));
+   cout << "Juego reiniciado.\n";
+}
+
+void signalHandler(int signum) {
+   bool gameCancelled = false;
+}
+
+void renderBoard() {
+   for (int i = 0; i < HEIGHT; ++i) {
+      for (int j = 0; j < WIDTH; ++j) {
+         cout << (board[i][j] ? "[]" : "..");
       }
+        cout << endl;
    }
+}
+
+Piece* createRandomPiece() {
+   return new Piece(static_cast<Tetromino>(rand() % TETROMINO_SHAPES.size()));
 }
--- tetrisProject.cpp
+++ tetrisProject.cpp
@@ -7,31 +7,78 @@
 using namespace std;
 
 const int WIDTH = 10, HEIGHT = 20;
+enum Tetromino {I, J, L, O, S, T, Z};
+
+const vector<vector<vector<int>>> TETROMINO_SHAPES = {
+   {{1, 1, 1, 1}},
+   {{1, 1}, {1, 1}},
+   {{0, 1, 0}, {1, 1, 1}},
+   {{1, 0, 0}, {1, 1, 1}},
+   {{0, 0, 1}, {1, 1, 1}},
+   {{0, 1, 1}, {1, 1, 0}},
+   {{1, 1, 0}, {0, 1, 1}}
+};
+
+struct Piece {
+   vector<vector<int>> shape;
+   int x, y;
+
+   Piece(Tetromino type)
+      : shape(TETROMINO_SHAPES[type]), x(WIDTH / 2 - shape[0].size() / 2), y(0) {}
+};
 
 vector<vector<int>> board(HEIGHT, vector<int>(WIDTH, 0));
-bool gameCancelled = false;
+
+bool gameCancelled = false; // Variable global para manejar la cancelación
+
+// Funciones
+void displayTitleScreen();
+void resetGame();
+void signalHandler(int signum);
+void renderBoard();
+Piece* createRandomPiece();
+
+int main() {
+   signal(SIGINT, signalHandler);  // Registra el manejador de señales
+   displayTitleScreen();
+   cin.get(); // Esperar Enter
+   resetGame();
+   cout << "Juego iniciado.\n";
+   while (!gameCancelled) {
+      renderBoard();
+      Piece* currentPiece = createRandomPiece();
+      delete currentPiece; // Liberar memoria para la pieza
+   }
+   cout << "Juego terminado.\n";
+   return 0;
+}
 
 void displayTitleScreen() {
-    cout << "=====================================\n"
+   cout  << "=====================================\n"
          << "=         BIENVENIDO A TETRIS       =\n"
          << "=====================================\n\n"
          << "Presiona Enter para comenzar...\n";
 }
-// sss
+
 void resetGame() {
-    board.assign(HEIGHT, vector<int>(WIDTH, 0));
-    cout << "Juego reiniciado.\n";
+   board.assign(HEIGHT, vector<int>(WIDTH, 0));
+   cout << "Juego reiniciado.\n";
 }
 
 void signalHandler(int signum) {
-    gameCancelled = true;
+   cout << "Juego cancelado con señal " << signum << ".\n";
+   gameCancelled = true;  // Detener el juego cuando se reciba una señal
 }
 
-int main() {
-    signal(SIGINT, signalHandler);
-    displayTitleScreen();
-    cin.get(); // Esperar Enter
-    resetGame();
-    cout << "Juego iniciado.\n";
-    return 0;
+void renderBoard() {
+   for (int i = 0; i < HEIGHT; ++i) {
+      for (int j = 0; j < WIDTH; ++j) {
+         cout << (board[i][j] ? "[]" : "..");
+      }
+      cout << endl;
+   }
+}
+
+Piece* createRandomPiece() {
+   return new Piece(static_cast<Tetromino>(rand() % TETROMINO_SHAPES.size()));
 }
