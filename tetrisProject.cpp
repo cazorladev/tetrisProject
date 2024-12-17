fab606b252c922bd1fbeea3db683c5b10efcbb4c
cazorladev <cazorladevnet@gmail.com>
2024-12-17 15:27:59 -0500

    Configuración inicial y estructura base

--- tetrisProject.cpp
+++ tetrisProject.cpp
@@ -2,43 +2,36 @@
 #include <vector>
 #include <ctime>
 #include <cstdlib>
+#include <csignal>
 
 using namespace std;
 
-// Configuración Inicial
 const int WIDTH = 10, HEIGHT = 20;
 
-// Enumeración para las piezas tetromino
-enum Tetromino {I, J, L, O, S, T, Z};
-
-struct Piece {
-   vector<vector<int>> shape;
-   int x, y;
-
-   Piece(Tetromino type) : x(WIDTH / 2), y(0) {}
-};
-
-// Variables globales
 vector<vector<int>> board(HEIGHT, vector<int>(WIDTH, 0));
+bool gameCancelled = false;
 
-// Funciones
-void displayBoard();
-
-int main() {
-
-   for (int i = 0; i < 10; i++) {
-      fill(board[i].begin(), board[i].end(), 0);
-   }
-   cout << "¡Bienvenido a Tetris!" << endl;
-   displayBoard();
-
-   return 0;
+void displayTitleScreen() {
+    cout << "=====================================\n"
+         << "=         BIENVENIDO A TETRIS       =\n"
+         << "=====================================\n\n"
+         << "Presiona Enter para comenzar...\n";
 }
 
-void displayBoard() {
-   for (int i = 0; i < HEIGHT; i++) {
-      for (int j = 0; j < WIDTH; j++) {
-         cout << board[i][j] << "..";
-      }
-   }
+void resetGame() {
+    board.assign(HEIGHT, vector<int>(WIDTH, 0));
+    cout << "Juego reiniciado.\n";
+}
+
+void signalHandler(int signum) {
+    gameCancelled = true;
+}
+
+int main() {
+    signal(SIGINT, signalHandler);
+    displayTitleScreen();
+    cin.get(); // Esperar Enter
+    resetGame();
+    cout << "Juego iniciado.\n";
+    return 0;
 }
